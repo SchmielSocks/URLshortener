ebde295d693e272012d1c7ccc00dfd2890591933
Lukas Collins <spoops31326@gmail.com>
2019-12-24 13:29:45 -0500

    Changed to get rid of generic sample project text. Added sample page and link panel.

--- FlaskWebProject1/FlaskWebProject1/views.py
+++ FlaskWebProject1/FlaskWebProject1/views.py
@@ -5,7 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app
-from FlaskWebProject1.urlGen import urlGen
+from FlaskWebProject1.urlGen import *
 
 @app.route('/')
 @app.route('/home')
@@ -13,6 +13,15 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
+        title='Playground Hub',
+        year=datetime.now().year,
+    )
+
+@app.route('/sample')
+def sample():
+    """Renders sample page."""
+    return render_template(
+        'sample.html',
+        title='Sample Project Page',
         year=datetime.now().year,
     )
